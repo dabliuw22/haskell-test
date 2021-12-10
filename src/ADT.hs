c4a943b56a251e79eb1f12e5b1994d18b7f6cc85
Will Leyton <wleylop22@gmail.com>
2021-12-10 09:49:19 -0500

    Update

--- src/ADT.hs
+++ src/ADT.hs
@@ -6,32 +6,37 @@ data Color = Red | Blue | Green deriving (Show, Eq)
 prettyPrint :: Color -> IO ()
 prettyPrint color =
   case color of
-    Red   -> print $ show Red
-    Blue  -> print $ show Blue
+    Red -> print $ show Red
+    Blue -> print $ show Blue
     Green -> print $ show Green
 
 -- product type
 -- data RGB = RGB Int Int Int deriving Show
-data RGB =
-  RGB {
-    red :: Int,
+data RGB = RGB
+  { red :: Int,
     green :: Int,
     blue :: Int
-  } deriving Eq
+  }
+  deriving (Eq)
 
 readRed :: RGB -> Int
 readRed (RGB r _ _) = r -- Pattern Matching
 
-data PrettyRGB = PrettyRGB { rgb :: RGB }
+newtype PrettyRGB = PrettyRGB {rgb :: RGB}
+
 --newtype PrettyRGB = PrettyRGB { rgb :: RGB }
 
 -- instance of TypeClass Show(PrettyRGB)
 instance Show PrettyRGB where
-  show pRgb = let pRgb' = rgb pRgb in
-    "RGB(red = " ++ show (red pRgb') ++
-    ", green = " ++ show (green pRgb') ++ ", " ++
-    ", blue = " ++ show (blue pRgb') ++
-    ")"
+  show pRgb =
+    let pRgb' = rgb pRgb
+     in "RGB(red = " ++ show (red pRgb')
+          ++ ", green = "
+          ++ show (green pRgb')
+          ++ ", "
+          ++ ", blue = "
+          ++ show (blue pRgb')
+          ++ ")"
 
   showsPrec = undefined
 
