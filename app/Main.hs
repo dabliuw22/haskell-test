7b71962f2514792ba9788d1d61798048ac748bcc
Will Leyton <wleylop22@gmail.com>
2021-12-16 08:22:33 -0500

    Update Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,13 @@
 module Main where
 
 import ADT
+import Control.Monad
+import Control.Monad.Except
 import DataStructures
 import Functions
 import Monads
 import PatternMatching
 import TypeClasses
-import Control.Monad
-import Control.Monad.Except
 
 readInts :: String -> [Int]
 -- words: Create a Array of String from String
@@ -15,15 +15,18 @@ readInts :: String -> [Int]
 readInts s = let ws = words s in map read ws
 
 r = head
---r t = fromS 
 
-minMax :: Ord a => [a] -> Maybe(a, a)
+--r t = fromS
+
+minMax :: Ord a => [a] -> Maybe (a, a)
 -- foldr: Reduction function, foldr(function, initValue, list)
 -- \: For lambda function
-minMax (head : tail) = Just $ foldr
-  (\number (min, max) -> (if number < min then number else min, if number > max then number else max))
-  (head, head)
-  tail
+minMax (head : tail) =
+  Just $
+    foldr
+      (\number (min, max) -> (if number < min then number else min, if number > max then number else max))
+      (head, head)
+      tail
 minMax _ = Nothing
 
 --let x = [1, 2, 3, 4, 5] in print(foldr (\a b -> a + b) 0 x)
@@ -42,10 +45,11 @@ loadFile = do
   print range
 
 main :: IO ()
-main = do 
+main = do
   e <- execute "hello4"
   print e
   md
+
 --main = safeDivIo 4 2 `catchError` (\e -> return 1) >>= print
 
 safeDivIo :: Int -> Int -> IO Int
@@ -53,6 +57,6 @@ safeDivIo a b = do
   return (a `div` b)
 
 safeDiv :: Int -> Int -> Maybe Int
-safeDiv a b =  do
+safeDiv a b = do
   guard $ b /= 0 -- guard requires Control.Monad, returns Pure Monad if condition is met, but Empty Monad.
   return (a `div` b)
